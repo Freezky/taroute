924510696f439ecfebf9f384bd2caadc236ace94
Freezky <kevyn.bele.binda@gmail.com>
2017-03-11 07:26:37 -0500

    Passport authentication support (local + FB OAuth)

--- config/env/development.js
+++ config/env/development.js
@@ -4,5 +4,11 @@
 
 module.exports = {
     db: 'mongodb://localhost/taRouteDev',
-    sessionSecret: 'developmentSessionSecret'
+    sessionSecret: 'developmentSessionSecret',
+    facebook: {
+        clientID: '733631093482865',
+        clientSecret: '5308158e6801d905148b87c25206da20',
+        callbackURL: 'http://localhost:3000/oauth/facebook/callback',
+        profileFields: ['id', 'emails', 'gender', 'link', 'locale', 'name', 'timezone', 'updated_time', 'verified']
+    }
 };
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,3 +1,14 @@
 /**
  * Created by kevynbele-binda on 2017-03-11.
  */
+
+var config = require('./config'),
+    mongoose = require('mongoose');
+
+module.exports = function() {
+    var db = mongoose.connect(config.db);
+
+    require('../app/models/user.server.model');
+
+    return db;
+};
