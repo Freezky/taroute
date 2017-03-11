1c76d5e6c179f835e9af8139ad3a1424f91b26ca
Freezky <kevyn.bele.binda@gmail.com>
2017-03-11 07:55:11 -0500

    Google OAuth supports v1

--- config/env/development.js
+++ config/env/development.js
@@ -10,5 +10,10 @@ module.exports = {
         clientSecret: '5308158e6801d905148b87c25206da20',
         callbackURL: 'http://localhost:3000/oauth/facebook/callback',
         profileFields: ['id', 'emails', 'gender', 'link', 'locale', 'name', 'timezone', 'updated_time', 'verified']
+    },
+    google: {
+        clientID: '529443776406-vj24g6st5lnqa8ir2doc9c7gf25s6kos.apps.googleusercontent.com',
+        clientSecret: 'dUfSFSNPVEwizEYzj95jChCc',
+        callbackURL: 'http://localhost:3000/oauth/google/callback.com'
     }
 };
